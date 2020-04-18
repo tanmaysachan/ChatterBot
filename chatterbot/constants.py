5709298fff986d01f3c6bd771eb08a8a72e9d694
Tanmay <tnmysachan@gmail.com>
2020-04-18 10:11:19 +0530

    increase text_man_length to 5000

--- chatterbot/constants.py
+++ chatterbot/constants.py
@@ -8,7 +8,7 @@ The number 255 is used because that is the maximum length of a char field
 in most databases. This value should be enforced on a per-model basis by
 the data model for each storage adapter.
 '''
-STATEMENT_TEXT_MAX_LENGTH = 255
+STATEMENT_TEXT_MAX_LENGTH = 5000
 
 '''
 The maximum length of characters that the text label of a conversation can contain.
